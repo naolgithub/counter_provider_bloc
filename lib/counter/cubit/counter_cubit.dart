c10c4d95b43648b90ac0f73db74a66628b92b384
naolgithub <naolcovered@gmail.com>
2022-04-01 14:49:37 +0300

    odd number added

--- lib/counter/cubit/counter_cubit.dart
+++ lib/counter/cubit/counter_cubit.dart
@@ -6,8 +6,12 @@ class CounterCubit extends Cubit<int> {
 
   /// Add 1 to the current state.
   void increment() => emit(state + 1);
+  
   /// Add 2 to the current state.
   void evenNumber() => emit(state+2);
+
+  /// Add 1 to the current state.
+  void oddNumber() => emit(state + 1);
 
   /// Subtract 1 from the current state.
   void decrement() => emit(state - 1);
