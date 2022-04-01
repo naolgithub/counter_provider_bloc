90af87dac5a09bf7674656329ee30f6d68f90c88
naolgithub <naolcovered@gmail.com>
2022-04-01 11:37:50 +0300

    void main updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'counter/counter_observer.dart';
 void main() {
+  //print('object');
   BlocOverrides.runZoned(
     () => runApp(const CounterApp()),
     blocObserver: CounterObserver(),
