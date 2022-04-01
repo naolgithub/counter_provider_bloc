0cbbdee7e11f622ad3ea9326d94f008f12547576
naolgithub <naolcovered@gmail.com>
2022-04-01 11:29:57 +0300

    main updated

---
+++ lib/app.dart
@@ -0,0 +1,6 @@
+import 'package:counter_provider_bloc/counter/view/counter_page.dart';
+import 'package:flutter/material.dart';
+
+class CounterApp extends MaterialApp {
+  const CounterApp({Key? key}) : super(key: key, home: const CounterPage());
+}
